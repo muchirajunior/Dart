153413b3ada2847a68ba363014fa9374ba5004ec
root <root@localhost.localdomain>
2021-03-30 13:23:06 +0300

    variables in dart

---
+++ variables1.dart
@@ -0,0 +1,35 @@
+/*
+variables in dart 
+int , bool, float, strings and var
+ */
+
+void main(){
+  
+  //integers
+  int number=20;
+  var num;//declare
+  num=10; //initialize
+  //var will be  assaigned the datatype by the comiler according to the value assaigned
+  
+  print(number);
+  print(num);
+  
+  //decimal numbers
+  double x=11.2;
+  print(x);
+  
+  
+  //boolean variable
+  bool isCool=true;
+  var schooling; //declaration
+  schooling=false;
+  print(isCool);
+  print(schooling);
+  
+  //strings
+  String name="MUCHIRA JUNIOR";
+  var myname='muchira junior'; 
+  print(name);
+  print(myname);
+  
+}
