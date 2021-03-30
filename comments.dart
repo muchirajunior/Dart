86b5fc7ee0033d2208b24fb64ae1ef27fe53cd20
root <root@localhost.localdomain>
2021-03-30 10:51:15 +0300

    comments on dart

---
+++ comments.dart
@@ -0,0 +1,18 @@
+/*
+ this is a multiple line comment 
+ 
+ it comments many lines
+
+ this is a hello Dart program
+ */
+
+void main(){
+  
+  //single line comment
+  
+  //printing out somenting on dart
+  print('hello muchira junior on dart');
+  
+  print(10/2); //doing arithimetic operation
+
+}
