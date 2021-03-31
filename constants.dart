951e1568249da7aa0c3b6e5f76c11154f1f3476c
root <root@localhost.localdomain>
2021-03-31 12:48:47 +0300

    using final and const contants

---
+++ constants.dart
@@ -0,0 +1,21 @@
+/**
+ * constants in dart
+ * using const
+ * usnig final keyword
+ */
+
+void main() {
+  
+  //final keyword
+  //final keyword is initialized on use
+  final name="MUCHIRA JUNIOR";
+  final String Place="Nyeri"; //adding a variable type is optional
+
+  //const keyword
+  //const keyword is initialized on compile time
+  const PI=3.14;
+  const double Gravity=9.8;
+
+  print('name is $name place  is $Place \n pi is $PI and gravitional force is $Gravity');
+
+}
