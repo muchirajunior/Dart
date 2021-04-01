41d1aef37502c025a0fc6a91b4c4f912d89abdec
root <root@localhost.localdomain>
2021-04-01 14:00:05 +0300

    expression fuctions in dart

---
+++ expnfuction.dart
@@ -0,0 +1,19 @@
+/**
+ * using short hand expression in dart
+ * this is to shorten fuctions that can be writtern as a single line of code
+ */
+
+void main(){
+  //execute fuction sum
+  sum(4, 5);
+
+  //execute fuction area using interpolation
+  print(" the area is ${area(4,5)}");
+
+}
+
+//using the => fut arrow to write a fuction, no use of {}  
+void sum(int a, int b) => print(" the sum of $a and $b is : ${a+b}");
+
+//after the => we write the return statement ommiting the return keyword
+int area(int length, int width) => length*width;
