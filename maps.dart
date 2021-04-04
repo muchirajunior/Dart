492df2f7112e6cbdf2db7f559994711dd663455a
root <root@localhost.localdomain>
2021-04-04 21:38:03 +0300

    maps in dart

---
+++ maps.dart
@@ -0,0 +1,18 @@
+/**
+ * maps in dart
+ * json format structures
+ */
+
+void main(){
+  //a example of map
+  var details={
+    "name":"MUCHIRA",
+    "numbers":[2,4,5],
+    "age":21
+  };
+
+  details["sex"]="male"; //adding an item
+
+
+  print(details);
+}
